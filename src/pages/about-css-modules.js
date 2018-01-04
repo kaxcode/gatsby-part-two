dc5ac9972df4533b7340b9e9148d2ab0502a52bc
Kenia Valladarez <kenia.valladarez@cloudreach.com>
2018-01-04 13:20:49 -0500

    Installed styled-components to try-out.

--- src/pages/about-css-modules.js
+++ src/pages/about-css-modules.js
@@ -1,35 +1,62 @@
-import React from "react"
-import styles from "./about-css-modules.module.css"
+import React from "react";
+import styled from "styled-components";
 
-import Container from "../components/container"
+import Container from "../components/container";
 
-const User = props =>
-  <div className={styles.user}>
-    <img src={props.avatar} className={styles.avatar} alt="" />
-    <div className={styles.description}>
-      <h2 className={styles.username}>
-        {props.username}
-      </h2>
-      <p className={styles.excerpt}>
-        {props.excerpt}
-      </p>
-    </div>
-  </div>
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto 12px auto;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
 
-export default () =>
+const Avatar = styled.img`
+  flex: 0 0 96px;
+  width: 96px;
+  height: 96px;
+  margin: 0;
+`;
+
+const Description = styled.div`
+  flex: 1;
+  margin-left: 18px;
+  padding: 12px;
+`;
+
+const Username = styled.h2`
+  margin: 0 0 12px 0;
+  padding: 0;
+`;
+
+const Excerpt = styled.p`
+  margin: 0;
+`;
+
+const User = props => (
+  <UserWrapper>
+    <Avatar src={props.avatar} alt="" />
+    <Description>
+      <Username>{props.username}</Username>
+      <Excerpt>{props.excerpt}</Excerpt>
+    </Description>
+  </UserWrapper>
+);
+
+export default () => (
   <Container>
-    <h1>About CSS Modules</h1>
-    <p>CSS Modules are cool</p>
+    <h1>About Styled Components</h1>
+    <p>Styled Components is lame</p>
     <User
       username="Jane Doe"
       avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
       excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     />
-
     <User
       username="Bob Smith"
       avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
       excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
     />
-
   </Container>
+);
